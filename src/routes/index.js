7213fafa2f9538163bbbc6506eba6d66497886d7
Natália Castilho <nataliacastilho26@gmail.com>
2024-10-14 16:31:59 -0300

    correção Screen telaChamada

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { TelaLogin } from '../../TelaLogin';
 import HorarioAulas from '../../HorarioAulas';
 import PainelInicial from '../../PainelInicial';
-import TelaChamada from '../../TelaChamada';
 
 
 const { Navigator, Screen } = createStackNavigator();
@@ -21,10 +20,9 @@ export function AppRoutes() {
       <Screen name="TelaLogin" component={TelaLogin} />
       <Screen name="HorarioAulas" component={HorarioAulas} />
       <Screen name='PainelInicial' component={PainelInicial}/>
-      <Screen name='TelaChamada' component={TelaChamada}/>
     </Navigator>
   );
-  
+
 }
 
 
