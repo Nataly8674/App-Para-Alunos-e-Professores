46b139f59b5146e48c4faa3c22fef608f0e4b430
Natália Castilho <nataliacastilho26@gmail.com>
2024-10-14 17:16:11 -0300

    Concertando Branch TelaChamada

--- HorarioAulas.js
+++ HorarioAulas.js
@@ -15,7 +15,7 @@ export default function HorarioAulas({ navigation }) {
         <View style={styles.headerContent}>
           <Text style={styles.textoHeader}>Horário das Aulas</Text>
           <TouchableOpacity onPress={() => navigation.navigate("PainelInicial")}>
-            <Image source={require('./assets/IMG/seta1.png')} style={[styles.seta]} />
+            <Image source={require('D:/Repositorios Git Hub/App-Para-Alunos-e-Professores/assets/IMG/seta1.png')} style={[styles.seta]} />
           </TouchableOpacity>
         </View>
       </View>
@@ -38,7 +38,7 @@ export default function HorarioAulas({ navigation }) {
           </Text>
         </TouchableOpacity>
 
-        <Image source={require('./assets/IMG/ImgLivro.png')} style={[styles.ImgLivro]} />
+        <Image source={require('D:/Repositorios Git Hub/App-Para-Alunos-e-Professores/assets/IMG/ImgLivro.png')} style={[styles.ImgLivro]} />
 
         {/* Conteúdo oculto */}
         {isExpanded && (
