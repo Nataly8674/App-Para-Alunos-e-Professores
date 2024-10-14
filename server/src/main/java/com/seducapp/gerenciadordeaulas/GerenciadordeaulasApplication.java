7d9b07823d0410ba39c774c923789df9996dfd7c
Nataly8674 <nataliacastilho26@gmail.com>
2024-10-14 19:47:15 -0300

    organizando pastas do projeto (servidor e app)

---
+++ server/src/main/java/com/seducapp/gerenciadordeaulas/GerenciadordeaulasApplication.java
@@ -0,0 +1,13 @@
+package com.seducapp.gerenciadordeaulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadordeaulasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadordeaulasApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/seducapp/gerenciadordeaulas/GerenciadordeaulasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seducapp.gerenciadordeaulas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadordeaulasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
