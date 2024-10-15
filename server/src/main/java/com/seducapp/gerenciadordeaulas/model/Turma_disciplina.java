8a063adbcdb0014256d6be539deb4f16250ade9b
Luan <luancortes208@gmail.com>
2024-10-15 19:19:03 -0300

    Turma_disciplina

---
+++ server/src/main/java/com/seducapp/gerenciadordeaulas/model/Turma_disciplina.java
@@ -0,0 +1,43 @@
+package com.seducapp.gerenciadordeaulas.model;
+
+import java.io.Serializable;
+
+import com.seducapp.gerenciadordeaulas.model.pk.Turma_disciplinaPk;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Turma_disciplina implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@EmbeddedId
+	private Turma_disciplinaPk id = new Turma_disciplinaPk();
+	
+	@ManyToOne
+    @JoinColumn(name = "id_Professor")
+    private Professor professor;	
+
+	public Turma_disciplina(Turma turma, Disciplina disciplina) {
+		id.setTurma(turma);
+		id.setDisciplina(disciplina);
+	}
+	
+	public Turma getTurma() {
+		return id.getTurma();
+	}
+	
+	public void setTurma(Turma turma) {
+		id.setTurma(turma);
+	}
+	
+	public Turma getDisciplina() {
+		return id.getTurma();
+	}
+	
+	public void setDisciplina(Disciplina disciplina) {
+		id.setDisciplina(disciplina);
+	}
+}
