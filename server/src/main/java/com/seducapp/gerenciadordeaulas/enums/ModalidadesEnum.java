a435484fcc7a380ba71545ff41ddd7fae4d1aeed
Luan <luancortes208@gmail.com>
2024-10-20 20:13:57 -0300

    Adicionando-fromString

--- server/src/main/java/com/seducapp/gerenciadordeaulas/enums/ModalidadesEnum.java
+++ server/src/main/java/com/seducapp/gerenciadordeaulas/enums/ModalidadesEnum.java
@@ -1,27 +1,34 @@
 package com.seducapp.gerenciadordeaulas.enums;
 
 public enum ModalidadesEnum {
-	INTEGRAL(1),
-	REGULAR(2);
+    INTEGRAL(1),
+    REGULAR(2);
 
-	private int code;
-	
-	ModalidadesEnum(int code) {
-		this.code = code;
-	}
-	
-	public int getCode() {
-		return code;
-	}
-	
-	public static ModalidadesEnum valueOf(int code) {
-		for(ModalidadesEnum x :ModalidadesEnum.values()) {
-			if(x.getCode() == code) {
-				return x;
-			}
-		}
-		throw new IllegalArgumentException("Invalid ModalidadesEnum code!");
-	}
-	
+    private int code;
+
+    ModalidadesEnum(int code) {
+        this.code = code;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public static ModalidadesEnum valueOfCode(int code) {
+        for (ModalidadesEnum x : ModalidadesEnum.values()) {
+            if (x.getCode() == code) {
+                return x;
+            }
+        }
+        throw new IllegalArgumentException("Invalid ModalidadesEnum code!");
+    }
+
+    public static ModalidadesEnum fromString(String modalidade) {
+        try {
+            return ModalidadesEnum.valueOf(modalidade.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new RuntimeException("Invalid ModalidadesEnum value: " + modalidade);
+        }
+    }
 }
- 
+
