93bed5802642d0a573a361d2c45299f31bb5e291
Luan <luancortes208@gmail.com>
2024-10-20 20:48:22 -0300

    Removendo-funçao-inutilizada

--- server/src/main/java/com/seducapp/gerenciadordeaulas/repository/AlunoRepository.java
+++ server/src/main/java/com/seducapp/gerenciadordeaulas/repository/AlunoRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.seducapp.gerenciadordeaulas.model.Aluno;
 
 public interface AlunoRepository extends JpaRepository<Aluno, Long>{
-
 }
