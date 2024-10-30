c68c458c47d20eb189c0251d9435c8193aa9c72d
Luan <luancortes208@gmail.com>
2024-10-30 01:21:30 -0300

    Deletando-controller-desnecessário-no-momento

--- server/src/main/java/com/seducapp/gerenciadordeaulas/controllers/AlunoController.java
+++
@@ -1,9 +0,0 @@
-package com.seducapp.gerenciadordeaulas.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/aluno")
-public class AlunoController {
-}
