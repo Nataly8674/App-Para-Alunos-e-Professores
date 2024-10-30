83ad8e9a97db1e9bbb362259adc97460c42c56b8
Luan <luancortes208@gmail.com>
2024-10-30 01:51:25 -0300

    modificação-desfeita

--- server/src/main/java/com/seducapp/gerenciadordeaulas/services/UsuarioService.java
+++ server/src/main/java/com/seducapp/gerenciadordeaulas/services/UsuarioService.java
@@ -1,11 +1,9 @@
 package com.seducapp.gerenciadordeaulas.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.seducapp.gerenciadordeaulas.model.Usuario;
 import com.seducapp.gerenciadordeaulas.repository.UsuarioRepository;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @Service
 public class UsuarioService {
@@ -13,21 +11,9 @@ public class UsuarioService {
     @Autowired
     private UsuarioRepository usuarioRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-
     // Busca um usuário pelo CPF
     public Usuario findByCpf(String cpf) {
         return usuarioRepository.findByCpf(cpf);
-    }
-
-    // Valida as credenciais do usuário
-    public boolean validateCredentials(String cpf, String password) {
-        Usuario usuario = usuarioRepository.findByCpf(cpf);
-        if (usuario == null) {
-            throw new UsernameNotFoundException("Usuário não encontrado com o CPF: " + cpf);
-        }
-        return passwordEncoder.matches(password, usuario.getPassword());
     }
 }
 
