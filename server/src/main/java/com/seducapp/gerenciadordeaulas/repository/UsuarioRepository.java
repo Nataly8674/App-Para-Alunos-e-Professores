c55b1b41d3b36df61ad3c93f3d49b5d42bd4a095
Luan <luancortes208@gmail.com>
2024-11-01 12:23:40 -0300

    Nenhuma-modificação-a-mais

--- server/src/main/java/com/seducapp/gerenciadordeaulas/repository/UsuarioRepository.java
+++ server/src/main/java/com/seducapp/gerenciadordeaulas/repository/UsuarioRepository.java
@@ -1,11 +1,9 @@
 package com.seducapp.gerenciadordeaulas.repository;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.seducapp.gerenciadordeaulas.model.Usuario;
 
-public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
-
-	Usuario findByCpf(String cpf);
-
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByCpf(String cpf);
 }
