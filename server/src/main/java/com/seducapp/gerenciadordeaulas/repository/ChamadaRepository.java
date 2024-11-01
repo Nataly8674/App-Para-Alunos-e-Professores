767953b5da015c34782badbc40fbba96d132f6f0
Luan <luancortes208@gmail.com>
2024-11-01 12:22:15 -0300

    Criando-método-existsByHorarioIdAndData

--- server/src/main/java/com/seducapp/gerenciadordeaulas/repository/ChamadaRepository.java
+++ server/src/main/java/com/seducapp/gerenciadordeaulas/repository/ChamadaRepository.java
@@ -1,9 +1,14 @@
 package com.seducapp.gerenciadordeaulas.repository;
 
+import java.time.LocalDate;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.seducapp.gerenciadordeaulas.model.Chamada;
 
-public interface ChamadaRepository extends JpaRepository<Chamada, Long>{
-
+public interface ChamadaRepository extends JpaRepository<Chamada, Long> {
+    @Query("SELECT COUNT(c) > 0 FROM Chamada c WHERE c.horario.id_horario = :horarioId AND c.data = :data")
+    boolean existsByHorarioIdAndData(@Param("horarioId") Long horarioId, @Param("data") LocalDate data);
 }
